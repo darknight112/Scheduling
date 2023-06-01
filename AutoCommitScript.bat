329b43c669525838fc60bb7fcc35fa4d52a07e05
darknight112 <hkalamri@gmail.com>
2023-06-01 09:54:56 +0400

    runs a job every five minutes.

---
+++ AutoCommitScript.bat
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd C:\Users\Lenovo\Desktop\Task-3  # Replace with the path to your repository
+
+# Add your files to the repository
+git add .
+
+# Commit the changes
+git commit -m "make a calculation that can calculate the total amount of the created order" --allow-empty
+
+# Push the changes to GitHub
+git push
---
+++ src/main/java/com/Scheduling/Scheduling/Scheduling.java
@@ -0,0 +1,12 @@
+package com.Scheduling.Scheduling;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduling {
+    @Scheduled(cron = " * */5 * * * *")
+    public void task(){
+        System.out.println("runs a job every five minutes.");
+    }
+}
